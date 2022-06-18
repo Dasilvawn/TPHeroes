fb066bf717de6cbe4244e32b703d8052a8c6ff0d
Walter <dasilvawn@gmail.com>
2022-06-18 11:42:35 -0300

    servidor levantado

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express ();
+const port = 3030
+
+app.get('/ ')
+
+
+
+
+
+
+app.listen (port, ()=>console.log("server running in http://localhost:" + port));
