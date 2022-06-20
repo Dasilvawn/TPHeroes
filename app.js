8e311da75b36367aaa1c2dab7956bbbb1c5afb8b
Walter <dasilvawn@gmail.com>
2022-06-20 16:52:56 -0300

    agregue Inicio a las rutas

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = 3030
 app.use(express.static('public')); 
  
 
-
 /*rutas*/
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
 app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname,'views','babbage.html')));
@@ -17,9 +16,6 @@ app.get('/hamilton', (req,res) => res.sendFile(path.join(__dirname,'views','hami
 app.get('/hopper', (req,res) => res.sendFile(path.join(__dirname,'views','hopper.html')));
 app.get('/lovelace', (req,res) => res.sendFile(path.join(__dirname,'views','lovelace.html')));
 app.get('/turing', (req,res) => res.sendFile(path.join(__dirname,'views','turing.html')));
-
-
-
 
 
 
